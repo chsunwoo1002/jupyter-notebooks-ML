a2bd2857eeee1b9c49750b29ea5702f9062d9f78
chsunwoo1002 <chsunwoo1002@gmail.com>
2021-09-04 10:40:00 -0700

    add functionality to go button(parsing data from canvas to 2d array

--- src/components/Sidebar/SidebarContainer.js
+++ src/components/Sidebar/SidebarContainer.js
@@ -5,24 +5,34 @@ import InputCanvas from "./InputCanvas";
 import "./SidebarContainer.css";
 
 const SidebarContainer = (props) => {
-  const [drawing, setDrawing] = useState("");
+  const [drawing, setDrawing] = useState(undefined);
 
   const getCanvas = (canvas) => {
     setDrawing(canvas);
   };
 
-  const clearDrawing = (props) => {
+  const clearDrawing = () => {
     if (!drawing) {
       return;
     }
     const ctx = drawing.getContext("2d");
     ctx.clearRect(0, 0, drawing.width, drawing.height);
   };
-
+  const parsingCanvas = () => {
+    if (!drawing) {
+      return;
+    }
+    const ctx = drawing.getContext("2d");
+    let canvasData = ctx.getImageData(0, 0, drawing.width, drawing.height);
+    // for (let i = 3; i < canvasData.width; i++){
+    //   for (let j = )
+    // }
+    console.log(canvasData);
+  };
   return (
     <div className="sidebar">
       <InputCanvas currentDrawing={getCanvas} />
-      <Button>Go</Button>
+      <Button onClick={parsingCanvas}>Go</Button>
       <Button onClick={clearDrawing}>Reset</Button>
     </div>
   );
