3166d9448794c24b3afbdc3b638e59e23efbbdda
chsunwoo1002 <chsunwoo1002@gmail.com>
2022-01-15 21:09:32 -0800

    add letter classification model and code

--- src/components/Sidebar/ResetButton.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import Button from "../../UI/Button";
-
-const ResetButton = (props) => {};
-
-export default ResetButton;
--- src/helpers/Canvas.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import "./Canvas.css";
-
-const Canvas = (props) => <canvas {...props} />;
-
-export default Canvas;
